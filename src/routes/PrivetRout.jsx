b598781bfd8ff34cc671f9c075c11777f3ca725f
FSHimel <shahriarfahim067@gmail.com>
2026-05-04 14:48:12 +0600

    privet router added

---
+++ src/routes/PrivetRout.jsx
@@ -0,0 +1,18 @@
+import { use } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { Navigate, useLocation } from "react-router";
+
+const PrivetRout = ({ children }) => {
+  const { user, loading } = use(AuthContext);
+  const location = useLocation();
+  console.log(location);
+  if (loading) {
+    return <span className="loading loading-spinner text-success "></span>;
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate state={location?.pathname} to={"/"}></Navigate>;
+};
+
+export default PrivetRout;
