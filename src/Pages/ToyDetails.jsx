f91dd2d4f53aec0bd53e7129077e324fd4545b31
FSHimel <shahriarfahim067@gmail.com>
2026-05-07 03:32:38 +0600

    toy details added

--- src/Pages/ToyDetails.jsx
+++ src/Pages/ToyDetails.jsx
@@ -1,13 +1,13 @@
 import { Link, useParams } from "react-router";
 import useToyData from "../Hooks/useToyData";
 import { toast } from "react-toastify";
+import ratingIMG from "../assets/icon-ratings.png";
 
 const ToyDetails = () => {
   const { id } = useParams();
   const { toys } = useToyData();
   const toy = toys.find((t) => String(t.toyId) === id);
   const {
-    toyId,
     toyName,
     sellerName,
     sellerEmail,
@@ -28,19 +28,41 @@ const ToyDetails = () => {
   };
   return (
     <div className="bg-[#e6fffa] min-h-screen grid">
-      <div className="card card-side bg-base-100 shadow-2xl">
-        <figure className="h-86">
-          <img src={pictureURL} alt="" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
-          <p>Click the button to watch on Jetflix app.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
+      <div className="shadow-2xl m-4 rounded-t-2xl rounded-b-xl">
+        <div>
+          <img
+            className="w-full h-80 object-cover overflow-hidden rounded-2xl"
+            src={pictureURL}
+            alt=""
+          />
+        </div>
+        <div className="mx-2 md:mx-5 m-5">
+          <h1 className="text-3xl font-bold">{toyName}</h1>
+          <h2 className="text-xl font-bold">
+            Category: <span className="text-black/50">{subCategory}</span>
+          </h2>
+          <p className="text-black/40 font-semibold mt-2">{description}</p>
+          <h2 className="text-xl font-bold mt-2">Price: {price}$</h2>
+          <h2 className="mt-4">Contact with Seller</h2>
+          <div className="border w-2/12 border-black/20"></div>
+          <h1 className="text-[16px] font-semibold text-black/30">
+            Name: {sellerName}
+          </h1>
+          <h1 className="text-[16px] font-semibold text-black/30">
+            Email: {sellerEmail}
+          </h1>
+        </div>
+        <div className="flex flex-wrap justify-between  mx-10 mb-5">
+          <div className="badge badge-outline text-blue-500 text-xl md:text-2xl md:p-4">
+            {availableQuantity} available
+          </div>
+
+          <div className="badge badge-outline flex items-center gap-1 text-xs md:text-sm md:p-4">
+            <img src={ratingIMG} className="h-3 md:h-6" alt="" />
+            <p className="text-xl md:text-2xl ">{rating}</p>
           </div>
         </div>
       </div>
-
       <div className="w-full max-w-md mx-auto shadow-2xl m-10 bg-[#e6fffa] rounded-lg">
         <div className="card-body bg-[#e6fffa] flex-1 rounded-xl">
           <form onSubmit={handleTryNow} className="flex flex-col h-full">
@@ -70,7 +92,7 @@ const ToyDetails = () => {
       <div className="mb-5">
         <Link
           to={"/"}
-          className="bg-[#99ddff] text-black btn btn-primary mt-10 font-bold flex items-center w-fit mx-auto"
+          className="bg-[#ffffb3] text-black btn btn-primary mt-10 font-bold flex items-center w-fit mx-auto"
         >
           Go Back
         </Link>
